8fab0471879d4acd7ffd089752e9d9a92cdd4bc0
alexis-anderson <aanderson9313@gmail.com>
2020-03-15 21:30:30 -0400

    fixed minor mistakes

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ server.get('/', (req, res) => {
     res.send('Great Job!');
 }); 
 
+// GET request to /users returns array of users
 server.get('/users', (req, res) => {
     db.find()
     .then(users => {
@@ -28,24 +29,44 @@ server.get('/users', (req, res) => {
     })
 });
 
+// when Post req is made, return the created object
 server.post('/users', (req, res) => {
     const userInfo = req.body;
     console.log(userInfo);
 
-    db.insert(userInfo)
-    .then(user => {
-        res.status(201).json({success:true, user})
-    })
-    .catch(err => {
-        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
-    })
+    if ( typeof userInfo.name === 'undefined' || typeof userInfo.bio === 'undefined') {
+
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."});
+    } else {
+        db.insert(userInfo)
+            .then(user => {
+            res.status(201).json({success:true, user})
+        })
+            .catch(err => {
+            res.status(500).json({errorMessage: 'There was an error while saving the user to the database', err});
+        });
+    }
+});
+
+server.get('/users/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(user => {
+            if(user) {
+                res.status(200).json({user});
+            }else {
+                res.status(404).json({message: 'The user with the specified ID does not exist.'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: 'The user information could not be retrieved.', err});
+        });
 });
 
 server.delete('/users/:id', (req, res) => {
-    const {id} = req.params;
-    db.remove(id)
-        .then(deleted => {
-           if(deleted) {
+    
+    db.remove(req.params.id)
+        .then(user => {
+           if(user) {
                res.status(204).end();
            } else {
                res.status(404).json({message: 'The user with the specified ID does not exist'})
@@ -60,20 +81,21 @@ server.put('/users/:id', (req, res) => {
     const {id} = req.params;
     const changes = req.body;
 
-    DataView.update(id, changes)
-    .then(updated => {
-        if(updated) {
-            res.status(200).json({success: true, updated});
-
-        } else if (updated !== changes.id) {
-            res.status(400).json({message: 'The user with the specified ID does not exist.'})
-        } else if (updated !== changes.name && changes.bio) {
-            res.status(400).json({errorMessage: 'Please provide name and bio for the user.'})
-        } else if (updated !== changes) {
-            res.status(500).json({})
-        }
-    })
-    .catch( err => {
-        res.status(500).json({errorMessage: 'The user could not be removed'})
-    })
+    
+    if (typeof userInfo.name === 'undefined' || typeof userInfo.bio === 'undefined') {
+        res.status(400).json({errorMessage: 'Please provide a name and bio for the user.'});
+    
+    } else {
+        db.update( id, userInfo)
+        .then(user => {
+            if(user) {
+                res.status(200).json({ success: true, user});
+            } else {
+                res.status(404).json({message: 'the user with the specified ID does not exist.'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'the user information could not be modified.', err});
+        });
+    }
 })
